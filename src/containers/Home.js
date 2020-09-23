f4cc55460fbe61e552863d1ccf3f051f0b77cb56
Simulation42 <c.alex0706@gmail.com>
2020-09-23 17:05:53 -0400

    reeeee

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Poke'Gram</h1>
+        <p>A simple Pokemon themed instagram clone</p>
+      </div>
+    </div>
+  );
+}
