4d99a3da740d77bdb4aefc554d12da2a1e2a941a
Simulation42 <c.alex0706@gmail.com>
2020-09-23 17:07:26 -0400

    reeeeeee

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
+import Routes from './Routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppContainer">
+      <div className="navbar">
+        <Link to="/" className="navBrand">Poke'Gram</Link>
+        <Link to='/signup' className="signUp">Signup</Link>
+        <Link to='/login' className="login">Login</Link>
+      </div>
+      <Routes />
     </div>
   );
 }
